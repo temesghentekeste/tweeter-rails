c85d9cd7d5c2bb2132e9fdf266573b5341375d1a
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-15 13:20:04 +0300

    Add gravatar and restrict links to create, update and delete tweets to logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 64})
+    if user
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
+    end
+  end
 end
